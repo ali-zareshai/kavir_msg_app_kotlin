e3af9db6f0898ce265b48063102e9966a17ff6b1
ali zareshahi <alititi96@gmail.com>
2019-12-30 21:17:51 +0330

    adde demo vavig layer

--- app/src/main/java/com/shafa/ali/kavir_msg/activity/CategoryActivity.java
+++ app/src/main/java/com/shafa/ali/kavir_msg/activity/CategoryActivity.java
@@ -2,6 +2,11 @@ package com.shafa.ali.kavir_msg.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,6 +14,7 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.GestureDetector;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -30,7 +36,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 
-public class CategoryActivity extends AppCompatActivity {
+public class CategoryActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private RecyclerView categoryRecycler;
     private CategoryAdapter categoryAdapter;
     private RecyclerView.LayoutManager layoutManager;
@@ -44,10 +50,21 @@ public class CategoryActivity extends AppCompatActivity {
         categoryRecycler =(RecyclerView)findViewById(R.id.category_recyclerview);
         loading = (SpinKitView)findViewById(R.id.spin_cat);
         mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-//        setSupportActionBar(mTopToolbar);
+        setSupportActionBar(mTopToolbar);
         ///
         layoutManager = new LinearLayoutManager(this);
         categoryRecycler.setLayoutManager(layoutManager);
+
+        //***************************
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, mTopToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //////***************************************
 
         //
         /*Create handle for the RetrofitInstance interface*/
@@ -98,6 +115,24 @@ public class CategoryActivity extends AppCompatActivity {
         categoryAdapter =new CategoryAdapter(getApplicationContext(),categoryModelList);
         categoryRecycler.setAdapter(categoryAdapter);
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        // Handle navigation view item clicks here.
+        int id = menuItem.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
 
